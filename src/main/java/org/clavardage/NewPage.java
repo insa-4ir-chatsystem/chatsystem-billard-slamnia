c21d042424fafe53beeff9fa389e17b40594ce38
Slamnia Enzo <slamnia@insa-20929>
2023-12-11 16:47:39 +0100

    beginning of the ui

--- src/main/java/org/clavardage/NewPage.java
+++ src/main/java/org/clavardage/NewPage.java
@@ -4,7 +4,7 @@ import java.awt.*;
 
 public class NewPage extends JFrame {
     NewPage() {
-        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Welcome");
         setSize(400, 200);
     }
