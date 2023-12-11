131a20e4bd85a05172ae912b8126ad70ef61ebaa
nbillard <nathanbillard12@gmail.com>
2023-12-11 15:28:24 +0100

    des tests

--- src/test/java/ChatSystem/MessagesBDDTests.java
+++ src/test/java/ChatSystem/MessagesBDDTests.java
@@ -3,9 +3,12 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 import org.clavardage.ChatSystem.*;
 
-public class DiscoverySystemTests {
+public class MessagesBDDTests {
 
     private static MessagesBDD bdd;
 
     @BeforeAll
+    public static void setUp() {
+        MessagesBDDTests.bdd = MessagesBDD.getInstance()
+    }
 }
